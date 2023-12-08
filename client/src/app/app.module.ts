78b2622e97bf94603f722434319ef497f1aad0d8
enzoy <enzoy@ENZO_DESKTOP>
2023-12-08 12:27:12 -0300

    added SSL to angular app

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,8 +1,12 @@
+
+
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [
     provideClientHydration()
