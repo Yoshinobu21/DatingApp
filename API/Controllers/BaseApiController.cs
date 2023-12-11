a153682065a0982564582758a911f0472dd43d0c
enzo hirai <enzo.hirai@fabritech.com.br>
2023-12-11 17:56:57 -0300

    Auth login working

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+
+
+[ApiController]
+[Route("api/[controller]")] 
+public class BaseApiController : ControllerBase
+{
+
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,6 @@
 using API.Data;
+using API.Interfaces;
+using API.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,7 +15,7 @@ opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 );
 
 builder.Services.AddCors();
-
+builder.Services.AddScoped<ITokenService, TokenService>();
 
 var app = builder.Build();
 
