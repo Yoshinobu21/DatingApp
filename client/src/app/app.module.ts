a089fac072299c8bf05818d740bc523deb0183c2
enzo hirai <enzo.hirai@fabritech.com.br>
2024-01-15 17:47:43 -0300

    date picking ngx

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -25,6 +25,9 @@ import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
+import { TextInputComponent } from './_forms/text-input/text-input.component';
+import { DatePickerComponent } from './_forms/date-picker/date-picker.component';
+
 
 @NgModule({
   declarations: [
@@ -42,6 +45,9 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
     MemberCardComponent,
     MemberEditComponent,
     PhotoEditorComponent,
+    TextInputComponent,
+    DatePickerComponent,
+
   ],
   imports: [
     BrowserModule,
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -15,21 +15,35 @@ export class RegisterComponent implements OnInit{
 
   registerForm: FormGroup = new FormGroup({})
 
-  constructor(private accountService: AccountService, private toastr: ToastrService) {}
+  constructor(private accountService: AccountService, private toastr: ToastrService, private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.initializeForm()
   }
 
   initializeForm() {
-    this.registerForm = new FormGroup({
-      username: new FormControl('Hello', Validators.required),
-      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
-      confirmPassword: new FormControl('', [Validators.required, Validators.])
+    this.registerForm = this.fb.group({
+      gender:['male'],
+      username:['', Validators.required],
+      knownAs:['', Validators.required],
+      dateOfBirth:['', Validators.required],
+      city:['', Validators.required],
+      country:['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPassword: ['', [Validators.required, this.matchValues('password')]]
+    })
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
     })
   }
 
-match
+matchValues(matchTo: string): ValidatorFn {
+  return (control: AbstractControl) => {
+    return control.value === control.parent?.get(matchTo)?.value  ? null : {notMatching: true}
+  }
+}
+
+
   register() {
     console.log(this.registerForm?.value)
     // this.accountService.register(this.model).subscribe({
