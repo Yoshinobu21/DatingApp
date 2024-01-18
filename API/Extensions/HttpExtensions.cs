040202d0e44d36d9136c80166f9c7a5d8ea73977
Enzo Hirai <enzoyoshinobu@gmail.com>
2024-01-18 00:05:28 -0300

    pagination

---
+++ API/Extensions/HttpExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+
+namespace API.Extensions;
+
+public static class HttpExtensions
+{
+    public static void addPaginationHeader(this HttpResponse response, PaginationHeader header)
+    {
+
+        var jsonOption = new JsonSerializerOptions{PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
+        response.Headers.Add("Pagination", JsonSerializer.Serialize(header, jsonOption));
+        response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+
+    }
+}   
