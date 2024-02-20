250eefd7b603602b939223ecb626308a624b447f
enzo hirai <enzo.hirai@fabritech.com.br>
2024-02-20 10:55:28 -0300

    .

---
+++ API/Controllers/LikeController.cs
@@ -0,0 +1,23 @@
+﻿using API.Controllers;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API;
+
+public class LikeController : BaseApiController
+{
+    private readonly IUserRepository _userRepository;
+    private readonly ILikeRepository _likeRepository;
+
+    public LikeController(IUserRepository userRepository, ILikeRepository likeRepository)
+    {
+        _userRepository = userRepository;
+        _likeRepository = likeRepository;
+    }
+
+    [HttpPost("{username}")]
+    public async Task<ActionResult>
+    
+
+
+}
